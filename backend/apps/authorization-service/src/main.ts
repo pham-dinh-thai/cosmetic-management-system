8de0dafd64cbd89a7988f01f064d7e621f894437
zafkiel <phamdinhthai2005@gmail.com>
2026-08-22 15:29:11 +0700

    feat(authorization): init authorization-service

---
+++ backend/apps/authorization-service/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AuthorizationServiceModule } from './authorization-service.module';
+import { AUTHORIZATION_SERVICE_PORT } from 'constants/ports';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AuthorizationServiceModule);
+
+  await app.listen(AUTHORIZATION_SERVICE_PORT);
+}
+bootstrap();
